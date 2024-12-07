387438afd914f949b0e83b114634bf81e1d63ebb
Marc Weller <20712966+Pbc69@users.noreply.github.com>
2024-12-07 17:26:55 +0100

    Update day5.php

--- 2024/day5.php
+++ 2024/day5.php
@@ -11,11 +11,14 @@ foreach ($lines as $line) {
     $line = trim($line);
     if (str_contains($line, "|")) {
         $split = explode("|", $line);
-        $inhibitRules[] = $split[1] . "|" . $split[0];
+        $set = $split[1] . "|" . $split[0];
+        $inhibitRules[$set] = $set;
     } elseif (str_contains($line, ",")) {
         $printSet[] = explode(",", $line);
     }
 }
+
+$falseSets = []; // for part2
 
 // remove false sets
 foreach($printSet as $x => $pages) {
@@ -24,7 +27,8 @@ foreach($printSet as $x => $pages) {
         $pos ++;
         for ($j = $pos; $j < count($pages); $j++) {
             $set = $pages[$i] . "|" . $pages[$j];
-            if (in_array($set, $inhibitRules)) {
+            if (isset($inhibitRules[$set])) {
+                $falseSets[] = $pages;
                 unset($printSet[$x]);
                 break 2;
             }
@@ -37,4 +41,36 @@ foreach($printSet as $pages) {
     $mid = $pages[floor(count($pages) / 2)];
     $sum += $mid;
 }
-echo "Sum: $sum\n";
+echo "Sum: $sum\n"; // 4766
+
+
+// Part 2
+
+foreach($falseSets as $x => &$pages) {
+
+    while (true) {
+        $pos = 0;
+        for ($i = 0; $i < count($pages); $i++) {
+            $pos ++;
+            for ($j = $pos; $j < count($pages); $j++) {
+                $set = $pages[$i] . "|" . $pages[$j];
+                if (isset($inhibitRules[$set])) {
+                    $t = $pages[$j];
+                    $pages[$j] = $pages[$i];
+                    $pages[$i] = $t;
+                    continue 3; // while, (restart fix loop)
+                }
+            }
+        }
+
+        break;
+    }
+}
+unset($pages);
+
+$sum = 0;
+foreach($falseSets as $pages) {
+    $mid = $pages[floor(count($pages) / 2)];
+    $sum += $mid;
+}
+echo "Sum: $sum\n"; // 6257
